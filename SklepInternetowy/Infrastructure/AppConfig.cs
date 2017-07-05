1658a170eee6242fef200ed8e96d340c08490197
Michal-Szopa <szopqa96@gmail.com>
2017-07-05 21:59:12 +0200

    Added config class to handle icon changes

---
+++ SklepInternetowy/Infrastructure/AppConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace SklepInternetowy.Infrastructure
+{
+
+	/// <summary>
+	/// Returns path of icon for categories 
+	/// </summary>
+	public class AppConfig
+	{
+		private static string _ikonyKategoriiFolderWzgledny = ConfigurationManager.AppSettings{"IkonyKategoriiFolder";
+
+		public static string IkonyKategoriiFolderWzgledny
+		{
+			get
+			{
+				return _ikonyKategoriiFolderWzgledny;
+			}
+		}
+	}
+}
